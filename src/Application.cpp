7c9c452dbf48936ad274815c06b9d94cd7966eaa
TheLegendarySpark <thelegendspk101@gmail.com>
2026-04-24 11:29:39 -0700

    test commit

--- src/Application.cpp
+++ src/Application.cpp
@@ -2,7 +2,11 @@
 using namespace bobcat;
 
 Application::Application() {
-    window = new Window(25, 75, 400, 400, "GUI App");
+    window = new Window(25, 75, 400, 400, "Paintshop");
+
+    textBox = new TextBox(0, 0, 100, 100, "Layout Order"); // related to layout order system
+
+    window->end();
 
     window->show();
 }
