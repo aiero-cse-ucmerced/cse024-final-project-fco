d084fe4f3d483100702689d611ae978fe7bb202c
Alpha N. Triz <39038022+TheLegendarySpark@users.noreply.github.com>
2026-04-25 15:19:42 -0700

    Include iostream and update window initialization
    Added iostream for console output and modified window initialization.

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,6 +1,14 @@
 #include <Application.h>
+#include <iostream>
+
+using namespace std;
+
 using namespace bobcat;
-    window = new Window(25, 75, 400, 400, "GUI App");
+    window = new Window(25, 75, 400, 
+
+
+    cout << "eh;gjoirvflkjgrfij" << endl;
+    
 
     window->show();
 }
