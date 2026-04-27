42b2567c3b7416964c4980a54e2a605f5393238b
TheLegendarySpark <abergado@ucmerced.edu>
2026-04-27 00:54:59 +0000

    made application executable

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,7 @@
 #include <Application.h>
 
 int main() {
-
     Application app;
 
-    cout << "something" << endl;
-    cout << "something i guess?" << endl;
-
-
-    cout << "maybe something?? << endl;
-
-
-    int i = 0;
-    while (i < 5) {
-        cout << to_string(i) << " is a number. something idk..." << endl;
-        i++;
-    };
-    
     return app.run();
 }
