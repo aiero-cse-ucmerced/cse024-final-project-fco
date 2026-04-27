954843a0bec11916d9d163d9ec56d86656161634
TheLegendarySpark <abergado@ucmerced.edu>
2026-04-27 00:54:21 +0000

    created enums

---
+++ inc/Enums.h
@@ -0,0 +1,32 @@
+#ifndef ENUMS_H
+#define ENUMS_H
+
+enum TOOL {
+    // core tools
+    PENCIL,
+    ERASER,
+    SELECTOR,
+
+    // shape tools
+    SHAPE_CIRCLE,
+    SHAPE_PENTAGON,
+    SHAPE_RECTANGLE,
+    SHAPE_STAR,
+    SHAPE_TRAPEZOID,
+    SHAPE_TRIANGLE,
+
+    // default tool
+    UNKNOWN
+};
+
+enum BASE_COLORS {
+    RED,
+    ORANGE,
+    PURPLE,
+    GREEN,
+    BLUE,
+    YELLOW,
+    BLACK,
+};
+
+#endif
