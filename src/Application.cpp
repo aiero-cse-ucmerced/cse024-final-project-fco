77cc60f6e77b5ce88450c7483c8c57a924b479cf
TheLegendarySpark <abergado@ucmerced.edu>
2026-04-27 00:54:47 +0000

    fixed application

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,14 +1,8 @@
 #include <Application.h>
-#include <iostream>
 
 using namespace std;
-
 using namespace bobcat;
-    window = new Window(25, 75, 400, 
-
-
-    cout << "eh;gjoirvflkjgrfij" << endl;
     
-
-    window->show();
+Application::Application() {
+    
 }
