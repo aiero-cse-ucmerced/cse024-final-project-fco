0cf82c8d31c134f23e45992c2842d1c981318f0d
TheLegendarySpark <abergado@ucmerced.edu>
2026-04-27 01:12:34 +0000

    created menubar

---
+++ inc/Menubar.h
@@ -0,0 +1,58 @@
+#ifndef MENUBAR_H
+#define MENUBAR_H
+
+#include <bobcat_ui/group.h>
+#include <bobcat_ui/menu.h>
+#include <bobcat_ui/window.h>
+
+namespace aiero {
+    class Menubar : public bobcat::Group {
+        bobcat::Menu* _obj;
+        std::vector<bobcat::MenuItem*> _items;
+
+        virtual void onSelection(bobcat::Widget* sender) = 0;
+        
+        public:
+            Menubar(bobcat::Window* parent) : Group(0, 0, parent->w(), parent->h()) {
+                _obj = new bobcat::Menu();
+                _obj->parent(parent);            
+                _obj->show();
+            };
+
+            ~Menubar() {
+                for (bobcat::MenuItem* mitem : _items)
+                    delete mitem;
+
+                _items.clear();
+
+                delete _obj;
+            }
+
+            bobcat::MenuItem* operator[](int index) {
+                if (index < 0 || _items.size() < index+1) throw "Non existent index";
+                return _items[index];
+            };
+
+            bobcat::MenuItem* operator[](std::string menuName) {
+                for (bobcat::MenuItem* mitem : _items) {
+                    if (mitem->label() == menuName)
+                        return mitem;
+                }
+                
+                throw "Non existent menu item " + menuName;
+            };
+
+            bobcat::MenuItem* addItem(std::string itemName) {
+                bobcat::MenuItem* mitem = new bobcat::MenuItem(itemName);
+                _obj->addItem(mitem);
+
+                ON_CLICK(mitem, Menubar::onSelection);
+                
+                return mitem;
+            }
+            
+    };
+
+}
+
+#endif
