69752598c8eef9df98760f0d367aecf8d7b5ee1d
TheLegendarySpark <abergado@ucmerced.edu>
2026-04-28 20:21:21 +0000

    added an all tools reference

---
+++ inc/Tools.h
@@ -0,0 +1,11 @@
+// AIERO-FINAL-PROJECT-FCO FILE (Do not remove tag)
+// Refer to aiero.space/final-project-fco
+
+
+#ifndef ALL_TOOLS_H
+#define ALL_TOOLS_H
+
+#include "tools/eraser.h"
+#include "tools/paintbrush.h"
+
+#endif
