f284faf778f36bc08e8b3347f22c04dec0648b2c
TheLegendarySpark <abergado@ucmerced.edu>
2026-04-28 19:30:13 +0000

    fix x, y, w, and h property not being initialized

--- inc/Toolbar.h
+++ inc/Toolbar.h
@@ -18,10 +18,10 @@ namespace aiero {
         std::vector<aiero::Tool*> tools;
         
         void _init(int x, int y, int w, int h) {
-            _x = 0;
-            _y = 0;
-            _w = 0;
-            _h = 0;
+            _x = x;
+            _y = y;
+            _w = w;
+            _h = h;
 
             _obj = new bobcat::Window(_x, _y, _w, _h, "");
             _obj->parent(nullptr);
