2d5651936a5487382af7117ed99e1ab70b6b2ab4
TheLegendarySpark <abergado@ucmerced.edu>
2026-04-28 20:17:23 +0000

    added core tools

---
+++ inc/tools/paintbrush.h
@@ -0,0 +1,33 @@
+// AIERO-FINAL-PROJECT-FCO FILE (Do not remove tag)
+// Refer to aiero.space/final-project-fco
+
+#ifndef PAINTBRUSH_H
+#define PAINTBRUSH_H
+
+#include "../Enums.h"
+#include "../Tool.h"
+
+class EraserTool : public aiero::Tool {
+    TOOL _name = SHAPE_CIRCLE;
+
+    public:
+        EraserTool() {
+
+        };
+
+        // optional
+        void onClick(int x, int y) override {};
+        void onMouseDown(int x, int y) override {};
+        void onMouseUp(int x, int y) override {};
+
+        // required
+        void _activate() override {
+
+        };
+
+        void _deactivate() override {
+
+        };
+};
+
+#endif
