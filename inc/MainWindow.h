4e44455921b181eb8dc9e1077f728bac6879db62
TheLegendarySpark <abergado@ucmerced.edu>
2026-04-28 20:17:09 +0000

    several changes to mainwindow

--- inc/MainWindow.h
+++ inc/MainWindow.h
@@ -7,15 +7,61 @@
 #include "Menubar.h"
 #include "Toolbar.h"
 #include <FL/Fl_Group.H>
+#include <bobcat_ui/bobcat_ui.h>
 #include <bobcat_ui/group.h>
+#include <bobcat_ui/menu.h>
 #include <bobcat_ui/window.h>
+#include <unordered_map>
 
 namespace aiero {
     class MainWindowMenubar : public aiero::Menubar {
+        bobcat::MenuItem* viewMenuTabToolbar;
+        bobcat::MenuItem* viewMenuTabColorPicker;
+        bobcat::MenuItem* viewMenuTabSize;
+        bobcat::MenuItem* viewMenuTabLayers;
+        
+        bobcat::MenuItem* layerMenuTabDeleteShape;
+        bobcat::MenuItem* layerMenuTabBringLayerFront;
+        bobcat::MenuItem* layerMenuTabBringLayerBack;
+        bobcat::MenuItem* layerMenuTabBringLayerUp;
+        bobcat::MenuItem* layerMenuTabBringLayerDown;
+        
+        bobcat::MenuItem* editMenuTabUndo;
+        bobcat::MenuItem* editMenuTabRedo;
+        bobcat::MenuItem* editMenuTabDeleteCurrentLayer;
+        bobcat::MenuItem* editMenuTabDeleteClearCanvas;
+
+        std::unordered_map<bobcat::MenuItem*, bobcat::Window*> linkedMenuItems;
+
+        void _onLinkVisibility(bobcat::Widget* menuItemClicked) {
+            if (linkedMenuItems.find((bobcat::MenuItem*) menuItemClicked) == linkedMenuItems.end()) return;
+            bobcat::Window* menuWindow = linkedMenuItems[(bobcat::MenuItem*) menuItemClicked];
+
+            
+        }
+
+        void _linkTabVisibility(bobcat::MenuItem* mitem, bobcat::Window* menuWindow) {
+            linkedMenuItems[mitem] = menuWindow;
+            ON_CLICK(mitem, MainWindowMenubar::_onLinkVisibility);
+        };
 
         public:
             MainWindowMenubar(int w, int h) : Menubar(w, h) {
+                viewMenuTabToolbar = addItem("View/Toolbar");
+                viewMenuTabColorPicker = addItem("View/Color Picker");
+                viewMenuTabSize = addItem("View/Size");
+                viewMenuTabLayers = addItem("View/Layers");
                 
+                layerMenuTabDeleteShape = addItem("Layers/Delete Shape");
+                layerMenuTabBringLayerFront = addItem("Layers/Layer Front");
+                layerMenuTabBringLayerBack = addItem("Layers/Layer Back");
+                layerMenuTabBringLayerUp = addItem("Layers/Layer Up");
+                layerMenuTabBringLayerDown = addItem("Layers/Layer Down");
+
+                editMenuTabUndo = addItem("Edit/Undo");
+                editMenuTabRedo = addItem("Edit/Redo");
+                editMenuTabDeleteCurrentLayer = addItem("Edit/Delete Current Layer");
+                editMenuTabDeleteClearCanvas = addItem("Edit/Clear Canvas");
             };
     };
 
