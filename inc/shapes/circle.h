5b5f7ff322b9fba9015164887c0263081cb78b67
TheLegendarySpark <abergado@ucmerced.edu>
2026-04-28 20:17:52 +0000

    renamed circle tool

--- inc/shapes/circle.h
+++ inc/shapes/circle.h
@@ -1,35 +1,33 @@
 // AIERO-FINAL-PROJECT-FCO FILE (Do not remove tag)
 // Refer to aiero.space/final-project-fco
 
-
 #ifndef CIRCLE_H
 #define CIRCLE_H
 
-#include "../Tool.h"
 #include "../Enums.h"
+#include "../Tool.h"
 
 class CircleTool : public aiero::Tool {
     TOOL _name = SHAPE_CIRCLE;
-    
-    public:
-        CircleTool() {
-            
-        };
 
-        // optional
-        void onClick() {};
-        void onMouseDown() {};
-        void onMouseUp();
+public:
+    CircleTool() {
 
-        // required
-        void _activate() {
-            
-        };
-        
-        void _deactivate() {
-            
-        };
+    };
 
+    // optional
+    void onClick(int x, int y) override {};
+    void onMouseDown(int x, int y) override {};
+    void onMouseUp(int x, int y) override {};
+
+    // required
+    void _activate() override {
+
+    };
+
+    void _deactivate() override {
+
+    };
 };
 
 #endif
