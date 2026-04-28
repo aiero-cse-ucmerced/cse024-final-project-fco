c7e4bbad28011975363aaa12ad94a4f84bcdf865
TheLegendarySpark <abergado@ucmerced.edu>
2026-04-28 20:23:38 +0000

    created canvas class (barely started)

---
+++ inc/Canvas.h
@@ -0,0 +1,17 @@
+// AIERO-FINAL-PROJECT-FCO FILE (Do not remove tag)
+// Refer to aiero.space/final-project-fco
+
+
+#ifndef CANVAS_H
+#define CANVAS_H
+
+#include <bobcat_ui/group.h>
+
+namespace aiero {
+
+    class Canvas : public bobcat::Group {
+        
+    }
+};
+
+#endif
