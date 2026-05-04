81258505ed5225062c27b7d79a85eebcf3da89e4
ajou345 <ajouwork@gmail.com>
2026-05-04 15:20:42 -0700

    Creating Rectangle

--- inc/shapers/rectangle.h
+++ inc/shapers/rectangle.h
@@ -1,40 +1,59 @@
 // AIERO-FINAL-PROJECT-FCO FILE (Do not remove tag)
 // Refer to aiero.space/final-project-fco
 
-
 #ifndef RECTANGLE_H
 #define RECTANGLE_H
 
 #include "../Tool.h"
 #include "../Enums.h"
-
 #include "../Shape.h"
+#include "../Color.h"
 #include "../tools/CoreTool.h"
 
 class Rectangle : public Shape {
-    public:
-        void draw() override;
-        
-        bool checkMouseBounds(float mouseX, float mouseY) const override {
-            return false;
-        };
+public:
+    Rectangle()
+        : color(0, 0, 0)
+    {
+        x = 0.0f;
+        y = 0.0f;
+        width  = 0.2f;
+        height = 0.2f;
+    }
 
+    Rectangle(float _x, float _y, float _width, float _height, Color _color)
+        : color(_color)
+    {
+        x = _x;
+        y = _y;
+        width  = _width;
+        height = _height;
+    }
+
+    void draw() override;
+
+    bool checkMouseBounds(float mouseX, float mouseY) const override {
+        return false;
+    }
+
+private:
+    float x;
+    float y;
+    float width;
+    float height;
+    Color color;
 };
 
 class RectangleTool : public aiero::CoreTool {
     TOOL _name = SHAPE_RECTANGLE;
 
-    public:
-        RectangleTool() {
-            // change whatever
-        };
+public:
+    RectangleTool() {}
 
-        // optional
-        void onMouseDown(float x, float y) override;
+    void onMouseDown(float x, float y) override;
 
-        // required
-        void _activate() override {};
-        void _deactivate() override {};
+    void _activate() override {}
+    void _deactivate() override {}
 };
 
 #endif
