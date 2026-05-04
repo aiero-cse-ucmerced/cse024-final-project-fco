429a8973dfed522a9f841278e7261e024527ceeb
ajou345 <ajouwork@gmail.com>
2026-05-04 14:43:38 -0700

    Changed wrong file

--- inc/Rectangle.h
+++ inc/Rectangle.h
@@ -1,35 +1,21 @@
-#include <GL/gl.h>
-#include "../inc/Color.h"
-#include "../inc/shapers/Rectangle.h"
-#include "../inc/shapers/rectangle.h"
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
 
-Rectangle::Rectangle()
-    : color(0, 0, 0)
-{
-    x = 0.0;
-    y = 0.0;
-    width  = 0.2;
-    height = 0.2;
-}
+#include "../Color.h"
+#include <GL/freeglut.h>
+
+class Rectangle{
+    float x;
+    float y;
+    float width;
+    float height;
+    Color color;
+
+public:
+    Rectangle();
+    Rectangle(float _x, float _y, float _width, float _height, Color _color);
+    void draw() const;
+};
 
 
-Rectangle::Rectangle(float _x, float _y, float _width, float _height, Color _color)
-    : color(_color)
-{
-    x = _x;
-    y = _y;
-    width = _width;
-    height = _height;
-    color = _color;
-}
-
-void Rectangle::draw() const{
-    glColor3f(color.r() / 255.0, color.g() / 255.0, color.b() / 255.0);
-
-    glBegin(GL_POLYGON);
-    glVertex2f(x - width/2, y + height/2);
-        glVertex2f(x + width/2, y + height/2);
-        glVertex2f(x + width/2, y - height/2);
-        glVertex2f(x - width/2, y - height/2);
-    glEnd();
-}
+#endif
