b57448de25026f2a0adad391bb6f929bf2f5ad4f
ajou345 <ajouwork@gmail.com>
2026-05-04 18:09:25 -0700

    Added Rectangle Draw

--- src/Shapes.cpp
+++ src/Shapes.cpp
@@ -37,7 +37,13 @@ void Pentagon::draw() {
 }
 
 void Rectangle::draw() {
-    
+    glColor3f(color.r() / 255.0, color.g() / 255.0, color.b() / 255.0);
+    glBegin(GL_POLYGON);
+    glVertex2f(x - width/2, y + height/2);
+        glVertex2f(x + width/2, y + height/2);
+        glVertex2f(x + width/2, y - height/2);
+        glVertex2f(x - width/2, y - height/2);
+    glEnd();
 }
 
 void Star::draw() {
