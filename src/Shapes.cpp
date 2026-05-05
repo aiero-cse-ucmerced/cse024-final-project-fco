d298cc0640c22e1a1a6571ce19299fc04321c527
ajou345 <ajouwork@gmail.com>
2026-05-05 00:11:12 -0700

    Added Star Draw

--- src/Shapes.cpp
+++ src/Shapes.cpp
@@ -72,7 +72,50 @@ void Rectangle::draw() {
 }
 
 void Star::draw() {
+    const BaseColor bColor = _color.toBase();
     
+    glColor3f(bColor.r, bColor.g, bColor.b);
+    
+    const int VERTICES = 10;
+    const float PI = 3.141592;
+    const float TWO_PI = 2 * PI;
+
+    const float startAngle = PI * 0.5;
+
+    //Fill star
+    glBegin(GL_POLYGON);
+    for(int i = 0; i < VERTICES; i ++){
+        float r;
+        if(i % 2 ==0){
+            r = outerRadius;
+        } else {
+            r = innerRadius;
+        }
+        //converted deg to rad
+        float angle = startAngle + i * (TWO_PI / float(VERTICES)); 
+        float vx = _x + r * cos(angle);
+        float vy = _y + r * sin(angle);
+        glVertex2f(vx, vy);  
+    }
+    glEnd();
+
+    // outline
+    glColor3f(0.0f, 0.0f, 0.0f);
+    glBegin(GL_LINE_LOOP);
+    for (int i = 0; i < VERTICES; ++i) {
+         float r;
+        if(i % 2 ==0){
+            r = outerRadius;
+        } else {
+            r = innerRadius;
+        }
+        //converted deg to rad
+        float angle = startAngle + i * (TWO_PI / float(VERTICES)); 
+        float vx = _x + r * cos(angle);
+        float vy = _y + r * sin(angle);
+        glVertex2f(vx, vy);
+    }
+    glEnd();
 }
 
 void Triangle::draw() {
