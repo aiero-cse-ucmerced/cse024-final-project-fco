adf103b262272b4c9806d0db3a98c8a64c0ba0f8
ajou345 <ajouwork@gmail.com>
2026-05-04 19:38:21 -0700

    Created Circle

--- inc/shapers/circle.h
+++ inc/shapers/circle.h
@@ -4,38 +4,53 @@
 #ifndef CIRCLE_H
 #define CIRCLE_H
 
-#include "../Enums.h"
 #include "../Tool.h"
-
+#include "../Enums.h"
 #include "../Shape.h"
-#include "../Application.h"
-
+#include "../Color.h"
 #include "../tools/CoreTool.h"
 
 class Circle : public Shape {
-    public:
-        void draw() override;
-        
-        bool checkMouseBounds(float mouseX, float mouseY) const override {
-            return false;
-        };
+public:
+    Circle()
+        : color(0, 0, 0)
+    {
+        x = 0.0;
+        y = 0.0;
+        radius = 25.0;
+    }
 
+    Circle(float _x, float _y, float _radius, Color _color)
+        : color(_color)
+    {
+        x = _x;
+        y = _y;
+        radius = _radius;
+    }
+
+    void draw() override;
+
+    bool checkMouseBounds(float mouseX, float mouseY) const override {
+
+        return false;
+    }
+
+private:
+    float x;
+    float y;
+    float radius;
+    Color color;
 };
 
 class CircleTool : public aiero::CoreTool {
     TOOL _name = SHAPE_CIRCLE;
 
 public:
-    CircleTool() {
-        // TODO
-    };;
-
-    // optional
+    CircleTool() {}
     void onMouseDown(float x, float y) override;
-
-    // required
-    void _activate() override {};
-    void _deactivate() override {};
+    
+    void _activate() override {}
+    void _deactivate() override {}
 };
 
 #endif
