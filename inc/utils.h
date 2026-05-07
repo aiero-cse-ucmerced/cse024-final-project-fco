012723b0b865196b6d297ac774e8971441d81419
TheLegendarySpark <abergado@ucmerced.edu>
2026-05-07 19:21:35 +0000

    created deep cloen vector for history

--- inc/utils.h
+++ inc/utils.h
@@ -7,6 +7,7 @@
 
 #include <cmath>
 #include <vector>
+#include <type_traits>
 
 namespace utils {
     template <typename A> // where A is a math data type (int, double, float, etc..)
@@ -38,6 +39,26 @@ namespace utils {
         
         return rhs;
     };
+
+
+    // (1) vector of type, (2) class type for copy constructor
+    // example: Shape*, Shape*
+    template <typename D>
+    std::vector<D> deepCloneVector(const std::vector<D>& lhs) {
+        using Pointee = std::remove_pointer_t<D>;
+
+        static_assert(std::is_polymorphic_v<Pointee>,
+                    "deepCloneVector expects a pointer to a polymorphic base");
+        
+        std::vector<D> rhs;
+        rhs.reserve(lhs.size());
+
+        for (auto p : lhs) {
+            rhs.push_back(p ? static_cast<D>(p->clone()) : nullptr);
+        }
+
+        return rhs;
+    }
 }
 
 #endif
