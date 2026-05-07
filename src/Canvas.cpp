944395adc4836e4b1f229b5e0d6e70bf22c1ff9d
TheLegendarySpark <abergado@ucmerced.edu>
2026-05-07 19:20:33 +0000

    canvas history

--- src/Canvas.cpp
+++ src/Canvas.cpp
@@ -17,15 +17,18 @@
         void deleteObjectsFromMousePosition(float mx, float my);
 
     EVENTS:
-        virtual void onClickEvent(bobcat::Widget* sender) {};
+        virtual _createHistory onClickEvent(bobcat::Widget* sender) {};
         virtual void onMouseUpEvent(bobcat::Widget* sender, float mouseX, float mouseY) {};
         virtual void onMouseDownEvent(bobcat::Widget* sender, float mouseX, float mouseY) {};
         virtual void onMouseDragEvent(bobcat::Widget* sender, float mouseX, float mouseY) {};
 */
 
 #include "Canvas.h"
+#include "Point.h"
 #include "Shape.h"
+#include "utils.h"
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 using namespace bobcat;
@@ -33,8 +36,9 @@ using namespace aiero;
 
 void Canvas::addShape(Shape* shape) {
     layers.push_back({0, {shape}});
-    redraw();
-    std::cout << "shape added -> canvas redraw" << std::endl;
+    _createHistory();
+    // redraw();
+    // std::cout << "shape added -> canvas redraw" << std::endl;
 }
 
 // POINTS TRACKER
@@ -49,16 +53,57 @@ void Canvas::stopTrackingPoints() {
     _pointsTracked = false;
 
     if (!ptsCollection.empty()) {
+        layers.push_back({0, utils::copyVector<Shape*, Point*>(ptsCollection)});
         // layers.push_back({0, utils::copyVector(ptsCollection)});
     }
 
     ptsCollection.clear();
+    
+    _createHistory();
+}
+
+void Canvas::addPoint(Point* pt) {
+    if (!_pointsTracked) return;
+    ptsCollection.push_back(pt);
+}
+
+void Canvas::deletePoint(Point* pt) {
+    if (ptsCollection.empty()) return;
+
+    if (ptsCollection.back() == pt) {
+        ptsCollection.pop_back();
+        return;
+    }
+    
+    auto it = std::find(ptsCollection.begin(), ptsCollection.end(), pt);
+    if (it == ptsCollection.end()) return;
+    
+    int index = it - ptsCollection.begin();
+
+    ptsCollection.erase(ptsCollection.begin() + index+1);
 }
 
 void Canvas::render() {
+    // int count = 0;
+    // int layerc = 0;
+    
     for (aiero::Layer l : layers) {
         for (Shape* s : l.shapes) {
             s->draw();
+            // count++;
+            // std::cout << "drawn shape?";
+        }
+        
+        // layerc++;
+    }
+
+    if (!ptsCollection.empty()) {
+        for (Point* pt : ptsCollection) {
+            pt->draw();
         }
     }
+
+    // std::cout << "drawn shapes (" + std::to_string(count) + ")" << std::endl;
+    // std::cout << "drawn layers (" + std::to_string(layerc) + ")" << std::endl;
+    // cout << "canvas redrawn?" << endl;
 }
